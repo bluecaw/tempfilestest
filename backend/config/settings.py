5ed42e7afb204cd285bc01ab64c1e7ea5f2d131e
bluecaw <passioncat007@gmail.com>
2026-09-25 08:07:41 +0900

    Add search and date filter functionality to reports API and React UI2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,6 +76,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     
     # Third party apps
     'rest_framework',
