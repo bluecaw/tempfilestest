6ce10d1732b6f3975a97b1f2344ced2147d2f7f2
bluecaw <passioncat007@gmail.com>
2026-09-24 16:41:41 +0900

    Fix channel layers for Upstash18

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -138,7 +138,7 @@ else:
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
